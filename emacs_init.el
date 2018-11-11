ac92aa568a4a03cdbe073e9ea730e2f24b527a08
illya <44945070+illya-von-einzbern@users.noreply.github.com>
2018-11-11 22:21:49 +0800

    Add files via upload

---
+++ emacs_init.el
@@ -0,0 +1,13 @@
+;; M-x eval-buffer plz
+
+(package-refresh-contents)
+(package-install 'zenburn-theme)
+(package-install 'magit)
+(package-install 'w3m)
+(package-install 'auto-complete)
+(package-install 'slime)
+(package-install 'helm)
+(package-install 'helm-projectile)
+(package-install 'window-numbering)
+(save-buffers-kill-terminal)
+
---
+++ server_init.sh
@@ -0,0 +1,53 @@
+# mkdir
+
+mkdir -p Codes
+mkdir -p Documents
+mkdir -p Softwares
+
+
+# installation
+sudo apt-get -y install mosh
+sudo apt-get -y install emacs
+sudo apt-get -y install sbcl
+sudo apt-get -y install w3m
+sudo apt-get -y install tmux
+sudo apt-get -y install wget
+sudo apt-get -y install git
+sudo apt-get -y install zsh
+
+
+# this is for zsh
+echo "exec zsh" >> .bashrc
+cp Downloads/server_upload/.zshrc .zshrc
+
+# this is for emacs
+cp Downloads/server_upload/.emacs .emacs
+
+sudo sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+emacs --eval '(progn (find-file "~/Downloads/server_upload/emacs_init.el") (eval-buffer))'
+
+
+
+# this is for quicklisp
+
+sbcl --non-interactive --load Downloads/server_upload/quicklisp.lisp
+
+cp Downloads/server_upload/slime-helper.el quicklisp/
+
+
+# this is for git
+
+sh Downloads/server_upload/git_init.sh
+
+
+# copy test file
+
+cp Downloads/server_upload/server_test.sh .
+
+
+
+# clean up
+rm -f server_init_remote.sh 
+
+
+
